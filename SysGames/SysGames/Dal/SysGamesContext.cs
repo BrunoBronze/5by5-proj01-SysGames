0de38e208236fa0d008b237bf2062cbe247149da
BrunoBronze <bruno_oliveirabronze@hotmail.com>
2021-03-24 14:27:36 -0300

    Commit Dal

---
+++ SysGames/SysGames/Dal/SysGamesContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SysGames.Dal
+{
+    public class SysGamesContext : DbContext
+    {
+        public SysGamesContext() : base("SysGamesContext")
+        {
+        }
+
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cartao> Cartoes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Videogame> Videogames { get; set; }
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Acessorio> Acessorios { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<Pagamento> Pagamentos { get; set; }
+        public DbSet<Carrinho> Carrinhos { get; set; }
+        public DbSet<ItemCarrinho> ItemsCarrinho { get; set; }
+    }
+}
---
+++ SysGames/SysGames/Dal/SysGamesInitializer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SysGames.Dal
+{
+    public class SysGamesInitializer : DropCreateDatabaseIfModelChanges<SysGamesContext>
+    {
+        protected override void Seed(SysGamesContext context)
+        {
+            var cliente = new List<Cliente>()
+            {
+                Id = 1,
+                CPF = 11111111111,
+                Nome = "Andressa",
+                Email = "andressa@gmail.com",
+                Senha = "1234",
+                Logradouro = "Rua das flores",
+                Numero = "1234",
+                Bairro = "Jardim das flores",
+                Complemento = "Casa",
+                CEP = "11222333",
+                Localidade = "Araraquara",
+                UF = "SP"
+            };
+
+            var videogames = new List<Videogame>()
+            {
+                new Videogame()
+                {
+                    Id = "1",
+                    Nome = "Playstation 5",
+                    Descricao = "HD 1TB, SSD 120GB",
+                    Valor = 5999.90,
+                    QtdEstoque = 5,
+                    Marca = "Sony",
+                    Modelo = "Edição com disco"
+                }
+            };
+
+            var jogos = new List<Jogo>()
+            {
+                new Jogo()
+                {
+                    Id = "1",
+                    Nome = "The last of us 2",
+                    Descricao = "Jogo foda",
+                    Valor = 399.90,
+                    QtdEstoque = 7,
+                    Genero = "Suspense"
+                }
+            };
+            
+            var acessorios = new List<Acessorio>()
+            {
+                new Acessorio()
+                {
+                    Id = "1",
+                    Nome = "Controle PS5",
+                    Descricao = "Jogo foda",
+                    Valor = 399.90,
+                    QtdEstoque = 7,
+                    Tipo = "PS5"
+                }
+            };
+        }
+    }
+}
