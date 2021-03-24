2a23dd70c2f3fc34f1168bc98398a3e79659a8fe
Andressa Jovino S <andressajovinos@gmail.com>
2021-03-24 14:03:26 -0300

    Criacao do Models

---
+++ SysGames/SysGames/Models/Videogame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SysGames.Models
+{
+	public class Videogame
+	{
+		public string Marca { get; set; }
+		public string Modelo { get; set; }
+	}
+}
