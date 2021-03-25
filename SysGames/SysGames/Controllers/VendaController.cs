defaaa98c9ec97d68b1998a4c4f2f05e03c1468c
Leonardo Larocca <leolrccv@gmail.com>
2021-03-25 15:39:08 -0300

    Regras no Edit Vendas

--- SysGames/SysGames/Controllers/VendaController.cs
+++ SysGames/SysGames/Controllers/VendaController.cs
@@ -6,36 +6,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SysGames.Controllers
-{
-    public class VendaController : Controller
-    {
+namespace SysGames.Controllers {
+    public class VendaController : Controller {
         private SysGamesContext db = new SysGamesContext();
         // GET: Venda
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View(db.Vendas.ToList());
         }
 
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Venda venda)
-        {
-            if ((venda.Previsao.Date > DateTime.Now.Date))
-            {
-                if (ModelState.IsValid)
-                {
+        public ActionResult Create(Venda venda) {
+            if (venda.Previsao.Date > DateTime.Now.Date) {
+                if (ModelState.IsValid) {
                     var cliente = db.Clientes.Find(venda.Pagamento.Carrinho.Cliente.ClienteID);
-                    if (cliente != null)
-                    {
+                    if (cliente != null) {
                         var produto = db.Produtos.Find(venda.Pagamento.Carrinho.Produto.ProdutoID);
-                        if (produto != null)
-                        {
+                        if (produto != null) {
                             venda.Pagamento.Carrinho.Cliente = cliente;
                             venda.Pagamento.Carrinho.Produto = produto;
                             venda.DataHora = DateTime.Now;
@@ -44,57 +35,59 @@ namespace SysGames.Controllers
                             ViewBag.Mensagem = "Produto adicionado com sucesso";
                             return RedirectToAction("Index");
                         }
-                        else
-                        {
+                        else {
                             ViewBag.Produto = "Produto ID nao existe";
                         }
                     }
-                    else
-                    {
+                    else {
                         ViewBag.Cliente = "Cliente ID nao existe";
                     }
                 }
             }
-            else
-            {
+            else {
                 ViewBag.Previsao = "Previsao deve ser uma data futura!";
             }
             return View(venda);
         }
 
-        public ActionResult Edit(int id)
-        {
+        public ActionResult Edit(int id) {
             return View(db.Vendas.First(v => v.VendaID == id));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Venda venda)
-        {
-            if (ModelState.IsValid)
-            {
-                var vendaUpdate = db.Vendas.First(v => v.VendaID == venda.VendaID);
-                vendaUpdate.Previsao = venda.Previsao;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+        public ActionResult Edit(Venda venda) {
+            if (venda.Previsao.Date > DateTime.Now.Date) {
+                if (ModelState.IsValid) {
+                    var cliente = db.Clientes.Find(venda.Pagamento.Carrinho.Cliente.ClienteID);
+                    if (cliente != null) {
+                        var vendaUpdate = db.Vendas.First(v => v.VendaID == venda.VendaID);
+                        vendaUpdate.Previsao = venda.Previsao;
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else {
+                        ViewBag.Cliente = "Cliente ID nao existe";
+                    }
+                }
+            }
+            else {
+                ViewBag.Previsao = "Previsao deve ser uma data futura!";
             }
             return View(venda);
         }
 
-        public ActionResult Details(int id)
-        {
+        public ActionResult Details(int id) {
             return View(db.Vendas.First(v => v.VendaID == id));
         }
 
-        public ActionResult Delete(int id)
-        {
+        public ActionResult Delete(int id) {
             return View(db.Vendas.First(v => v.VendaID == id));
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirm(int id)
-        {
+        public ActionResult DeleteConfirm(int id) {
             db.Vendas.Remove(db.Vendas.First(v => v.VendaID == id));
             db.SaveChanges();
             return RedirectToAction("Index");
